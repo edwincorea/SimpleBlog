c8ba89599b59f0c6403025135246226499919272
edwin <edwin@ASGARD>
2016-08-07 12:02:34 -0700

    Imported styles and scripts.

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Optimization;
+
+namespace SimpleBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //backend bundle styles
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            //frontend bundle styles
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/Site.css"));
+
+            //backend bundle scripts
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-3.1.0.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive")
+                .Include("~/scripts/bootstrap"));
+
+            //frontend bundle scripts
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-3.1.0.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+        }
+    }
+}
