124a0ec363a03c73d1880a0cfdab2088a499d16f
edwin <edwin@ASGARD>
2016-08-15 15:57:59 -0700

    Index action on post admin

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,5 +1,9 @@
 ﻿using System.Web.Mvc;
+using System.Linq;
+using NHibernate.Linq;
 using SimpleBlog.Infrastructure;
+using SimpleBlog.Models;
+using SimpleBlog.Areas.Admin.ViewModels;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
@@ -7,9 +11,21 @@ namespace SimpleBlog.Areas.Admin.Controllers
     [SelectedTab("posts")]
     public class PostsController: Controller
     {
-        public ActionResult Index()
+        private const int PostsPerPage = 5;
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            int totalPostCount = Database.Session.Query<Post>().Count();
+
+            var currentPostsPage = Database.Session.Query<Post>()
+                .OrderByDescending(c => c.CreatedAt)
+                .Skip((page - 1) * PostsPerPage)
+                .Take(PostsPerPage)
+                .ToList();
+
+            return View(new PostsIndex
+            {
+                Posts = new PagedData<Post>(currentPostsPage, totalPostCount, page, PostsPerPage)
+            });
         }
     }
 }
