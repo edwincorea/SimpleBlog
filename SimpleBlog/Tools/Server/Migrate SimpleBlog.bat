f539bfc40f6de82127a6370e65c155606b39c665
edwin <edwin@ASGARD>
2016-08-27 13:50:22 -0600

    Server migrations utility.

---
+++ SimpleBlog/Tools/Server/Migrate SimpleBlog.bat
@@ -0,0 +1,3 @@
+C:\Users\Administrator\Desktop\Software\FluentMigrator-1.6.2\Migrate.exe --db=mysql --target=C:\Websites\simpleblog.dev\bin\simpleblog.dll --configPath=C:\Websites\simpleblog.dev\Web.config -c=MainDb
+
+pause
